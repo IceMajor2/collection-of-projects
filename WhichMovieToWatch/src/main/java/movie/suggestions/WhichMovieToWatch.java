71de5b3ec54181e56e43958d6066ee5d8665eea1
IceMajor <icemajorr2@gmail.com>
2023-05-02 19:02:15 +0200

    Display titles

--- WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
+++ WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
@@ -1,6 +1,8 @@
 package movie.suggestions;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,10 +13,13 @@ public class WhichMovieToWatch {
     public static void main(String[] args) throws IOException {
         String url = "http://www.imdb.com/search/title/?title_type=feature&release_date=%d".formatted(1990);
         Document doc = Jsoup.connect(url).get();
-        var movies = doc.select("div.lister-item.mode-advanced").eachText();
-        for(String movie : movies) {
-            System.out.println(movie);
+        for(Element movie : doc.select("div.lister-item.mode-advanced")) {
+            List<String> h3s = movie.select("h3").stream()
+                    .map(Element::text)
+                    .collect(Collectors.toList());
+            for(String h3 : h3s) {
+                System.out.println(h3);
+            }
         }
-        
     }
 }
