591202cb482b002e724432e4a7829cd28f2915da
IceMajor <icemajorr2@gmail.com>
2023-05-02 18:24:49 +0200

    HTML parse of the main div class 'lister-item'

--- WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
+++ WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
@@ -1,9 +1,16 @@
-
 package movie.suggestions;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class WhichMovieToWatch {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        String url = "http://www.imdb.com/search/title/?title_type=feature&release_date=%d".formatted(1990);
+        Document doc = Jsoup.connect(url).get();
+        Element listerList = doc.select("div.lister-list").first();
+        System.out.println(listerList);
     }
 }
