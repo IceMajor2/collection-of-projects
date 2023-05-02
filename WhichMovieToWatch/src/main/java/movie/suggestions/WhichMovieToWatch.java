69930f5d2dee119c7d1de4e89c4ae90a549e4e4f
IceMajor <icemajorr2@gmail.com>
2023-05-02 18:46:12 +0200

    Print movies in 1990 (most popular from IMdB)

--- WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
+++ WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
@@ -4,13 +4,17 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WhichMovieToWatch {
 
     public static void main(String[] args) throws IOException {
         String url = "http://www.imdb.com/search/title/?title_type=feature&release_date=%d".formatted(1990);
         Document doc = Jsoup.connect(url).get();
-        Element listerList = doc.select("div.lister-list").first();
-        System.out.println(listerList);
+        var movies = doc.select("div.lister-item.mode-advanced").eachText();
+        for(String movie : movies) {
+            System.out.println(movie);
+        }
+        
     }
 }
