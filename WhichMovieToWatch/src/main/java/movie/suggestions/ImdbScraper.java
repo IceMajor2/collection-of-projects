67ad4be1c2763f695dd916472667747855e214e6
IceMajor <icemajorr2@gmail.com>
2023-05-03 11:06:36 +0200

    Year extracted

--- WhichMovieToWatch/src/main/java/movie/suggestions/ImdbScraper.java
+++ WhichMovieToWatch/src/main/java/movie/suggestions/ImdbScraper.java
@@ -2,6 +2,7 @@ package movie.suggestions;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,36 +10,25 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ImdbScraper {
-    
+
     public static void allTimePopular() throws IOException {
         // store movies in Map
-        Map<Integer, Movie> everPopular = new HashMap<>();
-        
+        Map<Integer, List<Movie>> everPopular = new HashMap<>();
+
         // get HTML parsed
         String url = "https://www.imdb.com/search/title/?title_type=feature&sort=num_votes,desc";
         Document doc = Jsoup.connect(url).get();
-        
+
         Elements moviesInfo = doc.select("div.lister-item.mode-advanced");
         int count = 0;
-        for(Element movie : moviesInfo) {
-            if(count == 50) {
+        for (Element movie : moviesInfo) {
+            if (count == 50) {
                 break;
             }
             Elements header = movie.select("h3");
             String title = header.select("a[href]").first().text();
-            System.out.println(title);
             count++;
+            String year = header.select("span.lister-item-year.text-muted.unbold").first().text();
         }
     }
 }
-//        
-//        Document doc = Jsoup.connect(url).get();
-//        for(Element movie : doc.select("div.lister-item.mode-advanced")) {
-//            
-//            List<Element> h3s = movie.select("h3").stream()
-//                    .collect(Collectors.toList());
-//            for(Element h3 : h3s) {
-//                Elements links = h3.select("a[href]");
-//                System.out.println(links.text());
-//            }
-//        }
