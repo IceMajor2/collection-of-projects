c18b77d069894f5c5961cb1c5aa5846176eab013
IceMajor <icemajorr2@gmail.com>
2023-05-03 11:38:59 +0200

    Rating parse

--- WhichMovieToWatch/src/main/java/movie/suggestions/ImdbScraper.java
+++ WhichMovieToWatch/src/main/java/movie/suggestions/ImdbScraper.java
@@ -29,6 +29,8 @@ public class ImdbScraper {
             String title = header.select("a[href]").first().text();
             String yearStr = header.select("span.lister-item-year.text-muted.unbold").first().text();
             int year = yearToInt(yearStr);
+            String rating = movie.select("[name='ir']").first().text();
+            System.out.println(rating);
             count++;
         }
     }
