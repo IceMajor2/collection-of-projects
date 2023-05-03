f0d520e9101228af93bf8c744b74784589d9b1f7
IceMajor <icemajorr2@gmail.com>
2023-05-03 10:33:54 +0200

    Printing only names of movies

--- WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
+++ WhichMovieToWatch/src/main/java/movie/suggestions/WhichMovieToWatch.java
@@ -14,11 +14,12 @@ public class WhichMovieToWatch {
         String url = "http://www.imdb.com/search/title/?title_type=feature&release_date=%d".formatted(1990);
         Document doc = Jsoup.connect(url).get();
         for(Element movie : doc.select("div.lister-item.mode-advanced")) {
-            List<String> h3s = movie.select("h3").stream()
-                    .map(Element::text)
+            
+            List<Element> h3s = movie.select("h3").stream()
                     .collect(Collectors.toList());
-            for(String h3 : h3s) {
-                System.out.println(h3);
+            for(Element h3 : h3s) {
+                Elements links = h3.select("a[href]");
+                System.out.println(links.text());
             }
         }
     }
