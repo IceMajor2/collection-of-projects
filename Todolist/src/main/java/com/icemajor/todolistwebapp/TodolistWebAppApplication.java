8ef3a039bc0b9fb0cf6db8d8554d8e4a9e71ad5d
IceMajor <icemajorr2@gmail.com>
2023-05-09 09:57:17 +0200

    First commit

---
+++ Todolist/src/main/java/com/icemajor/todolistwebapp/TodolistWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.icemajor.todolistwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistWebAppApplication.class, args);
+	}
+
+}
---
+++ Todolist/src/test/java/com/icemajor/todolistwebapp/TodolistWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icemajor.todolistwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
