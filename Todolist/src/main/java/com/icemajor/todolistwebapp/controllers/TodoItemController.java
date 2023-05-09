6cf9cc4d8603e80d584f570dbf89ff4b387967ab
IceMajor <icemajorr2@gmail.com>
2023-05-09 11:03:17 +0200

    Config component

--- Todolist/src/main/java/com/icemajor/todolistwebapp/controllers/TodoItemController.java
+++ Todolist/src/main/java/com/icemajor/todolistwebapp/controllers/TodoItemController.java
@@ -1,20 +1,26 @@
 package com.icemajor.todolistwebapp.controllers;
 
+import com.icemajor.todolistwebapp.repositories.TodoItemRepository;
 import org.springframework.stereotype.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TodoItemController {
     
+    
     private final Logger log = LoggerFactory.getLogger(TodoItemController.class);
+    @Autowired
+    private TodoItemRepository todoItemRepository;
     
     @GetMapping("")
     public ModelAndView index() {
         log.debug("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
         return modelAndView;
     }
 }
